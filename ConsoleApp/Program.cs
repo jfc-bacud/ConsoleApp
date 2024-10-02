93c4e0479cb54ddd09754a54f8fcaaab13ca25cb
23-0085c <23-0085c@CCL01-05>
2024-10-02 18:06:57 +0800

    IDKKK

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
@@ -15,124 +16,40 @@ namespace ConsoleApp
         static Queue<string> enemyMove = new Queue<string>();
         static int[,] boardOwners = new int[3, 3];
 
-
         static void Main(string[] args)
         {
             GenerateDefaultBoard();
+            int playerWin = 0;
+            int compWin = 0;
 
             while (true)
             {
+                bool turnDone = false;
                 Console.Clear();
-                DisplayBoard();
-                UserInput();
-            }
+                DisplayBoard(playerWin, compWin);
+                UserInput(out turnDone);
 
-        }
-
-
-        #region User Input
-
-        /// <summary>
-        /// User Input Process
-        /// </summary>
-        static void UserInput()
-        {
-            Console.Write("Player Move <row,column> : ");
-            string userInput = Console.ReadLine();
-            string inputMove = "";
-
-            if (IsInputValid(userInput, out inputMove))
-            {
-                PlayerMove(inputMove);
-            }
-            else
-            {
-                Console.WriteLine("\nAn error has occured, please input a valid value in the proper format");
-                Console.WriteLine("Input a key to continue...");
-                Console.ReadKey();
-            }
-
-        }
-
-        /// <summary>
-        /// Validates if user input meets the format presented
-        /// </summary>
-        /// <param name="userInput"></param>
-        /// <param name="inputMove"></param>
-        /// <returns></returns>
-        static bool IsInputValid(string userInput, out string inputMove)
-        {
-            if (userInput.Length != 0 && userInput.Length <= 3 && userInput[1] == ',')
-            {
-                string tempString = InputSplitter(userInput);
-
-                if (IsChoiceValid(tempString))
+                if (!turnDone)
                 {
-                    inputMove = tempString;
-                    return true;
-                }
-            }
-
-            inputMove = "";
-            return false;
-        }
-
-        /// <summary>
-        /// Splits the string to not include the ',' character
-        /// </summary>
-        /// <param name="userInput"></param>
-        /// <returns></returns>
-        static string InputSplitter(string userInput)
-        {
-            string tempString = "";
-            tempString += userInput[0];
-            tempString += userInput[2];
-
-            return tempString;
-        }
-
-        /// <summary>
-        /// Checks if the numbers present within the string exists in the current context of the board
-        /// </summary>
-        /// <param name="tempString"></param>
-        /// <returns></returns>
-        static bool IsChoiceValid(string tempString)
-        {
-
-            bool result = false;
-
-            for (int i = 0; i < 2; i++)
-            {
-                if (tempString[i] == '0' || tempString[i] == '1' || tempString[i] == '2')
-                {
-                    result = true;
+                    continue;
                 }
 
-                else
+                if (WhoWins() == 1)
                 {
-                    result = false;
-                    break;
+                    Win(playerWin,compWin);
+                    playerWin++;
+                    continue;
+                }
+
+                ComputerMove();
+                if (WhoWins() == 0)
+                {
+                    Win(playerWin, compWin);
+                    compWin++;
+                    continue;
                 }
             }
-
-            if (result)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
         }
-
-        #endregion
-
-        #region Board Display
-
-        /// <summary>
-        /// Generates default values for the board (everything should be false)
-        /// </summary>
         static void GenerateDefaultBoard()
         {
             for (int x = 0; x < defaultBoard.GetLength(0); x++)
@@ -145,12 +62,74 @@ namespace ConsoleApp
             }
         }
 
-        /// <summary>
-        /// Generates visual for the board
-        /// </summary>
-        static void DisplayBoard()
+        // 4 METHODS
+
+        #region User Input 
+        static void UserInput(out bool turnDone)
+        {
+            Console.Write("Player Move <row,column> : ");
+            string userInput = Console.ReadLine();
+            string inputMove = "";
+
+            if (IsInputValid(userInput, out inputMove))
+            {
+                PlayerMove(inputMove, out turnDone);
+            }
+            else
+            {
+                Console.WriteLine("\nAn error has occured, please input a valid value in the proper format");
+                Console.WriteLine("Input a key to continue...");
+                Console.ReadKey();
+                turnDone = false;
+            }
+        }
+        static bool IsInputValid(string userInput, out string inputMove)
+        {
+            if (userInput.Length != 0 && userInput.Length <= 3 && userInput[1] == ',')
+            {
+                string tempString = InputSplitter(userInput);
+                if (IsChoiceValid(tempString))
+                {
+                    inputMove = tempString;
+                    return true;
+                }
+            }
+            inputMove = "";
+            return false;
+        }
+        static string InputSplitter(string userInput)
+        {
+            string tempString = "";
+            tempString += userInput[0];
+            tempString += userInput[2];
+            return tempString;
+        }
+        static bool IsChoiceValid(string tempString)
+        {
+            bool result = false;
+            for (int i = 0; i < 2; i++)
+            {
+                if (tempString[i] == '0' || tempString[i] == '1' || tempString[i] == '2')
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        // 3 METHODS
+
+        #region Board Display
+        static void DisplayBoard(int playerWin, int compWin)
         {
             Console.WriteLine("\n");
+            Console.WriteLine($"   Player - {playerWin} Computer - {compWin}\n");
             Console.WriteLine("\t0     1     2");
 
             for (int x = 0; x < defaultBoard.GetLength(0); x++)
@@ -162,7 +141,7 @@ namespace ConsoleApp
                 {
                     char tempChar = ' ';
 
-                    if (IndexChecker(x,y,out tempChar))
+                    if (IndexChecker(x, y, out tempChar))
                     {
                         Console.Write($"  |  {tempChar}");
                     }
@@ -178,16 +157,6 @@ namespace ConsoleApp
             }
             Console.WriteLine("     +-----+-----+-----+\n");
         }
-
-        #endregion
-
-        /// <summary>
-        /// Index checker for display
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="tempChar"></param>
-        /// <returns></returns>
         static bool IndexChecker(int x, int y, out char tempChar)
         {
             if (defaultBoard[x, y] == true)
@@ -197,24 +166,38 @@ namespace ConsoleApp
             }
             else
             {
-                // goes to IndexOwner later...
                 tempChar = ' ';
                 return false;
-            }         
+            }
         }
+        static char IndexOwner(int x, int y)
+        {
+            if (boardOwners[x, y] == 0)
+            {
+                return 'X';
+            }
 
-        /// <summary>
-        /// Index checker for moves
-        /// </summary>
-        /// <param name="inputMove"></param>
-        /// <returns></returns>
-        /// 
+            return 'O';
+        }
+        #endregion
+
+        // 3 METHODS
+
+        #region Player Move
+        static void IndexRemover(string inputMove)
+        {
+            int rowIndex = int.Parse(inputMove) / 10;
+            int columnIndex = int.Parse(inputMove) % 10;
+
+            defaultBoard[rowIndex, columnIndex] = false;
+            boardOwners[rowIndex, columnIndex] = -1;
+        }
         static bool IndexChecker(string inputMove)
         {
-            int rowIndex = int.Parse(inputMove)/10;
-            int columnIndex = int.Parse(inputMove)%10;
+            int rowIndex = int.Parse(inputMove) / 10;
+            int columnIndex = int.Parse(inputMove) % 10;
 
-            if (defaultBoard[rowIndex, columnIndex] == false && boardOwners[rowIndex,columnIndex] == -1)
+            if (defaultBoard[rowIndex, columnIndex] == false && boardOwners[rowIndex, columnIndex] == -1)
             {
                 defaultBoard[rowIndex, columnIndex] = true;
                 boardOwners[rowIndex, columnIndex] = 1;
@@ -225,17 +208,7 @@ namespace ConsoleApp
                 return false;
             }
         }
-
-        static void IndexRemover(string inputMove)
-        {
-            int rowIndex = int.Parse(inputMove) / 10;
-            int columnIndex = int.Parse(inputMove) % 10;
-
-            defaultBoard[rowIndex, columnIndex] = false;
-            boardOwners[rowIndex, columnIndex] = -1;
-        }
-
-        static void PlayerMove(string inputMove)
+        static void PlayerMove(string inputMove, out bool turnDone)
         {
             if (IndexChecker(inputMove))
             {
@@ -249,24 +222,78 @@ namespace ConsoleApp
                     playerMove.Dequeue();
                     playerMove.Enqueue(inputMove);
                 }
+
+                turnDone = true;
             }
             else
             {
                 Console.WriteLine("\nSpecific index has been taken, please input an available index");
                 Console.WriteLine("Input any key to continue....");
                 Console.ReadKey();
+
+                turnDone = false;
             }
 
         }
+        #endregion
 
-        static char IndexOwner(int x, int y)
+        // 2 METHODS
+
+        #region Win Condition Met
+        static int WhoWins()
         {
-            if (boardOwners[x,y] == 0)
+            for (int i = 0; i < 2; i++)
             {
-                return 'X';
+                for (int x = 0; x < boardOwners.GetLength(0); x++)
+                {
+                    if (boardOwners[x, 0] == i && boardOwners[x, 1] == i && boardOwners[x, 2] == i)
+                    {
+                        return i;
+                    }
+                }
+
+                for (int y = 0; y < boardOwners.GetLength(1); y++)
+                {
+                    if (boardOwners[0, y] == i && boardOwners[1, y] == i && boardOwners[2, y] == i)
+                    {
+                        return i;
+                    }
+                }
+
+                if (boardOwners[0, 0] == i && boardOwners[1, 1] == i && boardOwners[2, 2] == i || boardOwners[0, 2] == i && boardOwners[1, 1] == i && boardOwners[0, 2] == i)
+                {
+                    return i;
+                }
             }
 
-            return 'O';
+            return -1;
         }
+        static void Win(int playerWin, int compWin)
+        {
+            Console.Clear();
+
+            if (WhoWins() == 1)
+            {
+
+                DisplayBoard(playerWin, compWin);
+                Console.WriteLine("\nYou Won!");
+                Console.ReadKey();
+            }
+
+            for (int x = 0; x < playerMove.Count; x++)
+            {
+                playerMove.Dequeue();
+            }
+            GenerateDefaultBoard();
+        }
+        #endregion
+
+        static void ComputerMove()
+        {
+            
+
+    
+        }
+
     }
 }
